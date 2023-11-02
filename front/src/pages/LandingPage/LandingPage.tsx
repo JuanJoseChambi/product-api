87a2416b98431533b1d40b84d54309fb90ecc3dc
Juan Jose <chambijuanjose05@gmail.com>
2023-11-01 23:15:42 -0300

    Feat/Texto Inicial Landing Page

--- front/src/pages/LandingPage/LandingPage.tsx
+++ front/src/pages/LandingPage/LandingPage.tsx
@@ -1,7 +1,7 @@
 
 function LandingPage() {
   return (
-    <div className="w-screen h-screen flex justify-center items-center bg-neutral-950 text-gray-200 text-5xl font-bold">LandingPage</div>
+    <div className="w-screen h-screen flex justify-center items-center bg-neutral-950 text-gray-200 text-5xl font-bold">Landing Page de Psw</div>
   )
 }
 
