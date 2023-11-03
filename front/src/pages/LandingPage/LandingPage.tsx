a25a64fccce8a7a5812f3f8a8b524ec983d716c6
Juan Jose <chambijuanjose05@gmail.com>
2023-11-03 19:23:47 -0300

    Feat/NavTestTailwind

--- front/src/pages/LandingPage/LandingPage.tsx
+++ front/src/pages/LandingPage/LandingPage.tsx
@@ -1,7 +1,21 @@
 
 function LandingPage() {
   return (
-    <div className="w-screen h-screen flex justify-center items-center bg-neutral-950 text-gray-200 text-5xl font-bold">Landing Page de Psw</div>
+    <header className="bg-zinc-800 w-screen h-screen flex justify-center items-start flex-row">
+      
+      <nav className="area bg-slate-300 flex justify-between items-center">
+        <div className="px-5 py-3 bg-lime-500">
+          <p></p>
+        </div>
+        <div className="" >
+          <ul className="px-5 py-3 bg-lime-500 flex justify-center">
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+      </nav>
+    </header>
   )
 }
 
