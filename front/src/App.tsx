c4c0156c7e798829b2cd69b6e17f18d7fcf33283
Juan Jose <chambijuanjose05@gmail.com>
2023-12-17 21:35:51 -0300

    Init Product api

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,4 +1,3 @@
-import './App.css'
 import LandingPage from './pages/LandingPage/LandingPage'
 
 function App() {
--- front/src/pages/LandingPage/LandingPage.tsx
+++ front/src/pages/LandingPage/LandingPage.tsx
@@ -1,20 +1,8 @@
 
 function LandingPage() {
   return (
-    <header className="bg-zinc-800 w-screen h-screen flex justify-center items-start flex-row">
+    <header className="">
       
-      <nav className="area bg-slate-300 flex justify-between items-center">
-        <div className="px-5 py-3 bg-lime-500">
-          <p></p>
-        </div>
-        <div className="" >
-          <ul className="px-5 py-3 bg-lime-500 flex justify-center">
-            <li></li>
-            <li></li>
-            <li></li>
-          </ul>
-        </div>
-      </nav>
     </header>
   )
 }
