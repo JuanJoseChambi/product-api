99cd15fcee3f84e6a47fc26ef0e37f7d2cbdfd9d
Juan Jose <chambijuanjose05@gmail.com>
2023-12-25 23:05:36 -0300

    Feat/Component Crads

---
+++ front/src/components/Cards/Cards.tsx
@@ -0,0 +1,46 @@
+import { Products } from "../../interfaces/interfaces"
+import { FilterProducts, PaginationProducts } from "../../pages/ControlApi/ControlApi"
+
+interface ArrayMap {
+    array: Products[] | FilterProducts | PaginationProducts;
+}
+
+function Cards({array}:ArrayMap) {
+  return (
+    <>
+    {Array.isArray(array) ? array.map((product, i) => (
+        <section key={product._id} className="w-[300px] h-[400px] flex flex-col justify-evenly items-start p-2 m-2 bg-redd-500">
+          <picture className="w-full h-[150px] flex justify-center items-center bg-blue-500">
+              <img className="w-[250px]" src="Image Not Found" alt="Image Not Found" ></img>
+          </picture>
+          <h1 key={product._id} className="font-semibold text-xl">{product.name} | {product.stock}</h1>
+
+          <p className="flex justify-start items-center text-xs font-light">M:{product.brand} | C:{product.category} | T:{product.type}</p>
+
+          <p className="text-sm font-light">{product.description}</p>
+
+          <span className="flex justify-start items-center text-xs font-light">{product.sizes.map((size) => (<p key={size} className="mx-1">{size}</p>))} | {product.available_colors.map((color) => (<p key={color} className="mx-1">{color}</p>))}</span>
+
+          <p className="font-medium text-xm">Price : {product.price} | Card {i+1}</p>
+        </section>
+      )):array.results.map((product, i) => (
+        <section key={product._id} className="w-[300px] h-[400px] flex flex-col justify-evenly items-start p-2 m-2 bg-redd-500">
+          <picture className="w-full h-[150px] flex justify-center items-center bg-blue-500">
+              <img className="w-[250px]" src="Image Not Found" alt="Image Not Found" ></img>
+          </picture>
+          <h1 key={product._id} className="font-semibold text-xl">{product.name} | {product.stock}</h1>
+
+          <p className="flex justify-start items-center text-xs font-light">M:{product.brand} | C:{product.category} | T:{product.type}</p>
+
+          <p className="text-sm font-light">{product.description}</p>
+
+          <span className="flex justify-start items-center text-xs font-light">{product.sizes.map((size) => (<p key={size} className="mx-1">{size}</p>))} | {product.available_colors.map((color) => (<p key={color} className="mx-1">{color}</p>))}</span>
+
+          <p className="font-medium text-xm">Price : {product.price} | Card {i+1}</p>
+        </section>
+      ))}
+    </>
+  )
+}
+
+export default Cards
