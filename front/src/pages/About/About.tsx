839e9c01cd0c2097c8b6a900e596ae909b8aaa24
Juan Jose <chambijuanjose05@gmail.com>
2023-12-28 21:18:28 -0300

    Feat/About - Terms - Buton Home

---
+++ front/src/pages/About/About.tsx
@@ -0,0 +1,36 @@
+
+function About() {
+  return (
+    <main className="w-[90%] mx-auto h-[680px]">
+        <section className="space-y-10 py-10">
+
+            <article className="space-y-3">
+                <h2 className="font-bold text-xl tracking-wider text-neutral-700">¿Quien eres?</h2>
+                <p className="font-light text-sm">Soy Juan José, un desarrollador Fullstack apasionado por crear experiencias web significativas. Disfruto desarrollando sitios web y siempre busco que tengan un propósito claro y útil. Mi interés también se extiende al crecimiento personal a través del aprendizaje continuo en el campo de la programación.</p>
+            </article>
+
+            <article className="space-y-3">
+                <h2 className="font-bold text-xl tracking-wider text-neutral-700">¿Por que Product Api?</h2>
+                <p className="font-light text-sm">Product API es un sitio web sencillo diseñado con el propósito de facilitar prácticas para desarrolladores. Este servicio proporciona una API de productos con un formato similar al de plataformas de comercio electrónico. Su enfoque se centra en ofrecer una herramienta práctica y valiosa para aquellos que se están formando en el desarrollo, especialmente en el ámbito de servicios relacionados como billeteras virtuales, practicas de seguridad, autenticacion y otros campos afines.</p>
+            </article>
+
+            <article className="space-y-3">
+                <h2 className="font-bold text-xl tracking-wider text-neutral-700">¿Cuáles son las características clave de esta API?</h2>
+                <p className="font-light text-sm">Ofrecemos funcionalidades como la obtención de información detallada de productos, filtrado por categorías, paginacion, entre otras.</p>
+            </article>
+
+            <article className="space-y-3">
+                <h2 className="font-bold text-xl tracking-wider text-neutral-700">¿Es gratuita la API?</h2>
+                <p className="font-light text-sm">Sí, actualmente la API es gratuita para su uso. Sin embargo, debes revisar los términos y condiciones para obtener información actualizada.</p>
+            </article>
+        
+            <article className="space-y-3">
+                <h2 className="font-bold text-xl tracking-wider text-neutral-700">¿Derechos de Autor?</h2>
+                <p className="font-light text-sm">Product Api esta creado con fines educativos y libre uso de la misma por lo cual los datos y las imágenes se utilizan sin derecho de propiedad y pertenecen a sus respectivos propietarios.</p>
+            </article>
+        </section>
+    </main>
+  )
+}
+
+export default About
