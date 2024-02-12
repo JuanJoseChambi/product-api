80cc035b4f483c623bc8be5307aa2aaddb040cd7
Juan Jose <chambijuanjose05@gmail.com>
2024-02-12 17:50:08 -0300

    Feat/ Logo PA in Loader

--- front/src/components/Loading/Loading.tsx
+++ front/src/components/Loading/Loading.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef } from "react"
+import PALogo from "../../assets/PALogo.png"
 
 interface LoadingProp {
     fadeSwitch:boolean | number;
@@ -29,8 +30,13 @@ function Loading({fadeSwitch, timeFade = 500, text = "Cargando"}:LoadingProp) {
 
   return (
     <div className={`fixed flex flex-col justify-center items-center top-0 w-full h-screen bg-[#fdfdfd]  ${fadeSwitch ? `opacity-0 overflow-y-visible` : "opacity-100 overflow-hidden"} transition-[transform_opacity_display] duration-700`} ref={LoadingRef}>
-        <i className='text-4xl mb-2 bx bx-loader-alt bx-spin' ></i>
+        <i className='text-4xl text-primaryColorViolet  mb-2 bx bx-loader-alt bx-spin' ></i>
         <h3>{text}</h3>
+
+        <picture className="w-[80px] h-[80px] absolute bottom-0  flex justify-center items-center bg-redd-500 pointer-events-none select-none">
+          <img src={PALogo} alt="PA" className="pointer-events-none select-none"/>
+        </picture>
+
     </div>
   )
 }
